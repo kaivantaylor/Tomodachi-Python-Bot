8f17bb0f811dec3dade8a64dc56bc6f3650bc9aa
martyjapilado <martyjapilado@gmail.com>
2019-04-12 14:21:33 -1000

    Added Poyo commands

--- Chat_Bot/commands.py
+++ Chat_Bot/commands.py
@@ -59,7 +59,35 @@ class Commands:
         
         await self.client.send_message(channel, user)
         await self.client.send_file(channel, image)
+    
+    @commands.command(pass_context = True)
+    async def poyo(self, ctx):
+        """Kirby is here to give some Poyos
+        """
+        channel = ctx.message.channel
+        rand_num = random.randint(1,5)
+
+        if rand_num == 1:
+            image = './images/badkirbdab.png'
+        elif rand_num == 2:
+            image = './images/Minekirb.jpg'
+        elif rand_num == 3:
+            image = './images/thick.gif'
+        elif rand_num == 4:
+            image = './images/korb.gif'
+        elif rand_num == 5:
+            image = './images/KirbyStahps.PNG'
+        elif rand_num == 6:
+            image = './images/ImSorryKirbyPls.PNG'
+        elif rand_num == 7:
+            image = './images/HAAAAAa.PNG'
+        else:
+			print("Pika")
+            image = './images/Pika.gif'
         
+        await self.client.send_message(channel)
+        await self.client.send_file(channel, image)
+		
     @commands.command()
     async def ping(self):
         """Give a "Pong!" back to the user."""
